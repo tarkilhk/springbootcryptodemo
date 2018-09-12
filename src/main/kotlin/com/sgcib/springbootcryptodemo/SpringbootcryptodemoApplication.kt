e5d2d5ed9693fa9de12511596577cb705a59d13b
tarkil <tarkil@gmail.com>
2018-09-12 14:03:04 +0800

    Initial commit

---
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/SpringbootcryptodemoApplication.kt
@@ -0,0 +1,11 @@
+package com.sgcib.springbootcryptodemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootcryptodemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootcryptodemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/sgcib/springbootcryptodemo/SpringbootcryptodemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.sgcib.springbootcryptodemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringbootcryptodemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
