98ce2473da9328b5200364ac40f06061b972a912
tarkil <tarkil@gmail.com>
2018-09-12 15:02:59 +0800

    test fake price

--- src/main/kotlin/com/sgcib/springbootcryptodemo/controller/marketController.kt
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/controller/marketController.kt
@@ -1,13 +1,21 @@
 package com.sgcib.springbootcryptodemo.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.sgcib.springbootcryptodemo.domain.Execution
+import com.sgcib.springbootcryptodemo.domain.Order
+import com.sgcib.springbootcryptodemo.service.MarketService
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/market")
-class marketController {
+class marketController(private val marketService: MarketService) {
 
-    @GetMapping("/price2")
-    fun getPrice() = 2.0
+    @GetMapping("/price")
+    fun getPrice() = marketService.getLastPrice()
+
+    @PostMapping("/execute")
+    fun execute(@RequestParam size: Int): Execution {
+        val order = Order(size)
+        return marketService.execute(order)
+    }
+
 }
