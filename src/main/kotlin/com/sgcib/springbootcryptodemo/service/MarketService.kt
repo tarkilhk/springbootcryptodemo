e728de4318ca347dc6875a1888b0ebbff842bde7
tarkil <tarkil@gmail.com>
2018-09-13 16:50:17 +0800

    Add price service

---
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/service/MarketService.kt
@@ -0,0 +1,9 @@
+package com.sgcib.springbootcryptodemo.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class MarketService {
+
+    fun getLastPrice() = (50_000..60_000).shuffled().last().toDouble()
+}
