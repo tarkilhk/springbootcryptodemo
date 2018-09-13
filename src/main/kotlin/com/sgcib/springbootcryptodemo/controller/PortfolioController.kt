01a5740ff42fadd840d0affb20aa5ba3bbe05f0d
tarkil <tarkil@gmail.com>
2018-09-13 11:13:33 +0800

    Add in-memory executions

---
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/controller/PortfolioController.kt
@@ -0,0 +1,16 @@
+package com.sgcib.springbootcryptodemo.controller
+
+import com.sgcib.springbootcryptodemo.service.PortfolioService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/portfolio")
+class PortfolioController(private val portfolioService: PortfolioService) {
+
+    @GetMapping("/executions")
+    fun getExecutions() = portfolioService.getExecutions()
+
+}
