2d9b56a3ea91b0e8e1c4b61b2be51c3f37199ece
tarkil <tarkil@gmail.com>
2018-09-14 10:30:21 +0800

    Add get executions

--- src/main/kotlin/com/sgcib/springbootcryptodemo/controller/MarketController.kt
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/controller/MarketController.kt
@@ -3,6 +3,7 @@ package com.sgcib.springbootcryptodemo.controller
 import com.sgcib.springbootcryptodemo.domain.Execution
 import com.sgcib.springbootcryptodemo.domain.Order
 import com.sgcib.springbootcryptodemo.service.MarketService
+import com.sgcib.springbootcryptodemo.service.PortfolioService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/market")
-class MarketController(private val marketService: MarketService) {
+class MarketController(
+        private val marketService: MarketService,
+        private val portfolioService: PortfolioService) {
 
     @GetMapping("/price")
     fun getPrice() = marketService.getLastPrice()
@@ -19,7 +22,9 @@ class MarketController(private val marketService: MarketService) {
     @PostMapping("/execute")
     fun execute(size: Int): Execution {
         val order = Order(size)
-        return marketService.execute(order)
+        val execution = marketService.execute(order)
+        portfolioService.addExecution(execution)
+        return execution
     }
 
 }
---
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/service/PortfolioService.kt
@@ -0,0 +1,15 @@
+package com.sgcib.springbootcryptodemo.service
+
+import com.sgcib.springbootcryptodemo.domain.Execution
+import org.springframework.stereotype.Service
+
+@Service
+class PortfolioService {
+
+    private val executions = mutableListOf<Execution>()
+
+    fun getExecutions() = executions
+
+    fun addExecution(execution: Execution) = executions.add(execution)
+
+}
