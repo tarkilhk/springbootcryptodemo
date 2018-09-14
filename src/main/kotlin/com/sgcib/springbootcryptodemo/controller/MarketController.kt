94a0a0e1b9e3dec58dcc1295badb39053291115c
tarkil <tarkil@gmail.com>
2018-09-14 10:23:52 +0800

    Add execute service

--- src/main/kotlin/com/sgcib/springbootcryptodemo/controller/MarketController.kt
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/controller/MarketController.kt
@@ -1,7 +1,10 @@
 package com.sgcib.springbootcryptodemo.controller
 
+import com.sgcib.springbootcryptodemo.domain.Execution
+import com.sgcib.springbootcryptodemo.domain.Order
 import com.sgcib.springbootcryptodemo.service.MarketService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +15,11 @@ class MarketController(private val marketService: MarketService) {
 
     @GetMapping("/price")
     fun getPrice() = marketService.getLastPrice()
+
+    @PostMapping("/execute")
+    fun execute(size: Int): Execution {
+        val order = Order(size)
+        return marketService.execute(order)
+    }
 
 }
