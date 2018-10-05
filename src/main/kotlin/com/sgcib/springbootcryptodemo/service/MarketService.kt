b3779024ae058faae145ebbb31ee100ef3874b13
tarkil <tarkil@gmail.com>
2018-10-05 18:19:35 +0800

    init commit, get price

---
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/service/MarketService.kt
@@ -0,0 +1,10 @@
+package com.sgcib.springbootcryptodemo.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class MarketService {
+
+    fun getLastPrice() = (50_000..60_000).shuffled().last().toDouble()
+
+}
