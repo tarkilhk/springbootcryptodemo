c4d0ecf7ee96d74d5b7ff5d1b038a83f2eb7ce28
tarkil <tarkil@gmail.com>
2018-10-05 11:34:21 +0800

    add pnl

--- src/main/kotlin/com/sgcib/springbootcryptodemo/service/PortfolioService.kt
+++ src/main/kotlin/com/sgcib/springbootcryptodemo/service/PortfolioService.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class PortfolioService {
+class PortfolioService(private val marketService: MarketService) {
 
     private val executions = mutableListOf<Execution>()
 
@@ -14,4 +14,15 @@ class PortfolioService {
 
     fun addExecution(execution: Execution) = executions.add(execution)
 
+    fun calculatePnl():Double {
+        // market value portfolio - cost porfolio (what i paid)
+        return calculateMarketValue() - calculateCost()
+    }
+
+    fun calculateCost() = executions.map { it.order.size * it.price }.sum()
+
+    fun calculateMarketValue():Double {
+        val currentPrice = marketService.getLastPrice()
+        return executions.map { it.order.size * currentPrice }.sum()
+    }
 }
