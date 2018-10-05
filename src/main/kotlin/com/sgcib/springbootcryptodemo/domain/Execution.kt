3a76f209c36f11b51717d62582e0cb7651b50ac9
tarkil <tarkil@gmail.com>
2018-10-05 10:45:35 +0800

    Revert

--- src/main/kotlin/com/sgcib/springbootcryptodemo/domain/Execution.kt
+++
@@ -1,6 +0,0 @@
-package com.sgcib.springbootcryptodemo.domain
-
-class Execution(
-        val order: Order,
-        val price: Double
-)
--- src/test/kotlin/com/sgcib/springbootcryptodemo/service/PortfolioServiceTest.kt
+++ src/test/kotlin/com/sgcib/springbootcryptodemo/service/PortfolioServiceTest.kt
@@ -1,28 +1,27 @@
-package com.sgcib.springbootcryptodemo.service
-
-import com.sgcib.springbootcryptodemo.domain.Execution
-import com.sgcib.springbootcryptodemo.domain.Order
-import io.mockk.every
-import io.mockk.mockk
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-
-internal class PortfolioServiceTest {
-    private val marketService: MarketService = mockk()
-    private val portfolioService : PortfolioService = PortfolioService(marketService)
-
-    @BeforeEach
-    fun setUp() {
-        every { marketService.getLastPrice() } returns 200.0
-        portfolioService.addExecution(Execution(Order(10), 100.0))
-        portfolioService.addExecution(Execution(Order(20), 100.0))
-        portfolioService.addExecution(Execution(Order(20), 100.0))
-    }
-
-    @Test
-    fun `Test compute pnl, expect compute for all product`() {
-        val actual = portfolioService.getCalculatePnl()
-        assertEquals(5000.0, actual)
-    }
-}
+//package com.sgcib.springbootcryptodemo.service
+//
+//import io.mockk.every
+//import io.mockk.mockk
+//import org.junit.jupiter.api.Assertions.assertEquals
+//import org.junit.jupiter.api.BeforeEach
+//import org.junit.jupiter.api.Test
+//import org.junit.jupiter.api.parallel.Execution
+//
+//internal class PortfolioServiceTest {
+//    private val marketService: MarketService = mockk()
+//    private val portfolioService : PortfolioService = PortfolioService(marketService)
+//
+//    @BeforeEach
+//    fun setUp() {
+//        every { marketService.getLastPrice() } returns 200.0
+//        portfolioService.addExecution(Execution(Order(10), 100.0))
+//        portfolioService.addExecution(Execution(Order(20), 100.0))
+//        portfolioService.addExecution(Execution(Order(20), 100.0))
+//    }
+//
+//    @Test
+//    fun `Test compute pnl, expect compute for all product`() {
+//        val actual = portfolioService.getCalculatePnl()
+//        assertEquals(5000.0, actual)
+//    }
+//}
